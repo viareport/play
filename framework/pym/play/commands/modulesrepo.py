4d67a5caae2c9ca40cba73986631330a035f37f5
Benoît Caudesaygues <bcaudesaygues@viareport.com>
2014-09-19 17:49:55 +0200

    erreur de merge

--- framework/pym/play/commands/modulesrepo.py
+++ framework/pym/play/commands/modulesrepo.py
@@ -326,7 +326,7 @@ def build(app, args, env):
     manifestF.close()
 
     zip = zipfile.ZipFile(os.path.join(dist_dir, '%s.zip' % mv), 'w', zipfile.ZIP_STORED)
-    for (dirpath, dirnames, filenames) in os.walk(app.path):        
+    for (dirpath, dirnames, filenames) in os.walk(app.path, followlinks=True):        
         if dirpath.find(os.sep + '.') > -1 or dirpath.find('/tmp/') > -1 or dirpath.find('/test-result/') > -1 or dirpath.find('/logs/') > -1 or dirpath.find('/eclipse/') > -1 or dirpath.endswith('/test-result') or dirpath.endswith('/logs') or dirpath.endswith('/eclipse') or dirpath.endswith('/nbproject') or dirpath.find('node_modules') > -1 :
             continue
         if dirpath.startswith(os.path.join(app.path, 'modules')):
